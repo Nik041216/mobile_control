115fbf4ac9b45dc1479a731bc7f72cb27f073ad6
Bobr <bobrnikita1@gmail.com>
2025-03-20 13:11:42 +0300

    небольшая правка

--- scr/BD/auth.py
+++ scr/BD/auth.py
@@ -12,10 +12,10 @@ import scr.BD.bd_users.api_user
 async def check_user_credentials(login, password, page):
     employee = scr.BD.bd_users.api_user.check_user(login, password)
     if employee:
-        await scr.BD.bd_users.api_user.start_websocket(login, password, employee['employee_id'])
         scr.BD.bd_users.local.create_bd.local_user_db()
         scr.navigation_apps.navigations.create_route(page)
         if employee['privileges'] == 2:
+            await scr.BD.bd_users.api_user.start_websocket(login, password, employee['employee_id'])
             scr.BD.bd_users.local.insert_bd.insert_bd_user(
                 employee['employee_id'], employee['login'], password,
                 employee['privileges'], employee['first_name'], employee['last_name'], page
