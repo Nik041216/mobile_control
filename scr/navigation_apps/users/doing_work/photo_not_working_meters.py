48572b4fbd7d86ea87597774937d2d3e78d65167
Bobr <bobrnikita1@gmail.com>
2025-04-28 10:35:06 +0300

    смена с просроченого на в исполнении и новая функция для неработающих счетчиков

--- scr/navigation_apps/users/doing_work/photo_not_working_meters.py
+++ scr/navigation_apps/users/doing_work/photo_not_working_meters.py
@@ -1,10 +1,12 @@
 import flet as ft
 import scr.BD.bd_users.local.insert_bd
+import scr.BD.bd_users.local.update_bd
 import scr.func
 import scr.navigation_apps.users.doing_work.chose_meters
 from scr.components.loading import LoadingManager
 import base64
 import os
+import datetime
 
 
 def add_photo(page, id_task, where, container1, meter_id):
@@ -102,6 +104,8 @@ def add_photo(page, id_task, where, container1, meter_id):
     )
 
     def on_click_save(e):
+        today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        scr.BD.bd_users.local.update_bd.update_not_working_meters(str(today), id_task, meter_id)
         scr.navigation_apps.users.doing_work.chose_meters.show_meters_data(page, id_task, where, container1)
         page.close(dlg)
         page.update()
